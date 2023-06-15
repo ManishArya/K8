81b92b6e3a631a272c65a0443ee41971892c2776
manish <testproject100@outlook.com>
2023-06-15 22:56:01 +0530

    first commit

--- index.js
+++ index.js
@@ -11,17 +11,21 @@ const citySchema = new mongoose.Schema({
 const model = mongoose.model("city", citySchema);
 
 const app = express();
-const connectionString = "mongodb://localhost:27017";
+app.set('view engine', 'ejs')
+
+const connectionString = process.env.connectionString;
 
 connect(connectionString, {
-  dbName: process.env.dbName || "Sample",
+  dbName: process.env.dbName,
+  user: process.env.user,
+  pass: process.env.pass,
   useUnifiedTopology: true,
   useNewUrlParser: true,
 }).then(() => console.info("connected to db"));
 
 app.get("/api", async (req, res) => {
-  const cities = await model.find({}, { _id: 0 });
-  res.send(cities);
+ const cities = await model.find({}, { _id: 0 });
+ res.render('index',{ cities})
 });
 
 const port = process.env.PORT || 3000;
